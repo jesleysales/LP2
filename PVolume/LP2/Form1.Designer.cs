c2ffc3ae06c6baaa13a508f881c1397116523c98
jesleysales <115020737+jesleysales@users.noreply.github.com>
2023-03-13 17:37:16 -0300

    Add files via upload

---
+++ PVolume/LP2/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace LP2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vRaio = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.vAltura = new System.Windows.Forms.TextBox();
+            this.vVolume = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // vRaio
+            // 
+            this.vRaio.Location = new System.Drawing.Point(86, 67);
+            this.vRaio.Name = "vRaio";
+            this.vRaio.Size = new System.Drawing.Size(166, 20);
+            this.vRaio.TabIndex = 0;
+            this.vRaio.Validated += new System.EventHandler(this.vRaio_Validated);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Raio";
+            // 
+            // vAltura
+            // 
+            this.vAltura.Location = new System.Drawing.Point(86, 115);
+            this.vAltura.Name = "vAltura";
+            this.vAltura.Size = new System.Drawing.Size(166, 20);
+            this.vAltura.TabIndex = 2;
+            this.vAltura.Validated += new System.EventHandler(this.vAltura_Validated);
+            // 
+            // vVolume
+            // 
+            this.vVolume.Location = new System.Drawing.Point(86, 165);
+            this.vVolume.Name = "vVolume";
+            this.vVolume.ReadOnly = true;
+            this.vVolume.Size = new System.Drawing.Size(166, 20);
+            this.vVolume.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Altura";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Volume";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(86, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(177, 251);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Fechar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 299);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.vVolume);
+            this.Controls.Add(this.vAltura);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.vRaio);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox vRaio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox vAltura;
+        private System.Windows.Forms.TextBox vVolume;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PVolume/LP2/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LP2
+{
+    public partial class Form1 : Form
+    {
+        double Raio, Altura, Volume;
+
+        private void vAltura_Validated(object sender, EventArgs e)
+        {
+            if (!double.TryParse(vAltura.Text, out Altura))
+                MessageBox.Show("Altura Inválida");
+            else
+                        if (Altura <= 0)
+                MessageBox.Show("A altura deve ser maior que zero.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Volume = Math.PI * Math.Pow(Raio, 2) * Altura;
+            vVolume.Text = Volume.ToString("N2");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void vRaio_Validated(object sender, EventArgs e)
+        {
+            if (!double.TryParse(vRaio.Text, out Raio))
+                MessageBox.Show("Raio Inválido");
+            else
+                       if (Raio <= 0)
+                MessageBox.Show("O raio deve ser maior que zero.");
+
+        }
+    }
+}
+    
+
+
+        
+   
